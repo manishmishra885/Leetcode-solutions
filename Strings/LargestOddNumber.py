d271e9c1ae45dc3149334447384ddbba60d51090
Manish Mishra <mmanishmishra885@gmail.com>
2026-06-05 14:11:37 +0530

    Update LargestOddNumber.py

--- Strings/LargestOddNumber.py
+++ Strings/LargestOddNumber.py
@@ -1,7 +1,8 @@
 class Solution:
         def largestOddNumber(self, num: str) -> str:
-                #start, stop , iteration .... so that would be (4-1 , -1,-1)
+                        
+                        #start, stop , iteration .... so that would be (4-1 , -1,-1)
                         for i in range(len(num)-1, -1, -1):
                                     if int(num[i]) % 2 != 0 :
                                                     return num[:i+1]
-                                                            return ""
+                        return ""
