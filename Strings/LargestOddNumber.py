9f3812243317d58b26bd028edf9dc6ec6229ef98
Manish Mishra <mmanishmishra885@gmail.com>
2026-06-05 14:09:26 +0530

    Create LargestOddNumber.py

---
+++ Strings/LargestOddNumber.py
@@ -0,0 +1,7 @@
+class Solution:
+        def largestOddNumber(self, num: str) -> str:
+                #start, stop , iteration .... so that would be (4-1 , -1,-1)
+                        for i in range(len(num)-1, -1, -1):
+                                    if int(num[i]) % 2 != 0 :
+                                                    return num[:i+1]
+                                                            return ""
