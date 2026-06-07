cc1316cff55ba0d737aaba4725f3ac792cd96cb7
Manish Mishra <mmanishmishra885@gmail.com>
2026-06-07 16:45:01 +0530

    Create Rotate_Array.py

---
+++ Arrays_Easy/Rotate_Array.py
@@ -0,0 +1,9 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        k = k % len(nums)
+        nums[:] = nums[-k:] + nums[:-k]
+        return (nums)
+        # while k > 0:
+        #     nums[:] = nums[-1:] + nums[:-1]
+        #     k = k-1
+        # return nums
