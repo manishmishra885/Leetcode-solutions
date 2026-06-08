83a724fa2f1d3709040327deef7a2847524622d0
Manish Mishra <mmanishmishra885@gmail.com>
2026-06-08 15:59:20 +0530

    Solved

--- Arrays_Easy/Single_Number.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def singleNumber(self, nums: List[int]) -> int:
-        counter = {}
-        for i in nums:
-            counter[i] = counter.get(i,0) +1
-        
-        for key,value in counter.items():
-            if value == 1 :
-                return(key)
