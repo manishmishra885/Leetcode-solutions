d4e9c7d1f3c2e8fb1af8e575b516c066fc060d99
Manish Mishra <mmanishmishra885@gmail.com>
2026-06-08 15:58:17 +0530

    Delete Check_Sorted_And_Rotated.py

--- Arrays_Easy/Check_Sorted_And_Rotated.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def check(self, nums: List[int]) -> bool:
-        n = len(nums)
-        nums2 = nums + nums
-        for i in range (n):
-            window = nums2[i:i+n]
-            if window == sorted(window):
-                return True
-        return False
