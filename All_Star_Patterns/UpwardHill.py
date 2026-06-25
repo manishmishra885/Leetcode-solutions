cbd9e69a31bdc79b2995e1a14d9f96d6352e0d3c
Manish Mishra <mmanishmishra885@gmail.com>
2026-06-25 17:58:51 +0530

    Update UpwardHill.py

--- All_Star_Patterns/UpwardHill.py
+++ All_Star_Patterns/UpwardHill.py
@@ -1,2 +1,10 @@
 n = 5 
-for i in range
+for i in range(n):
+    for j in range(n-i):
+        print(" ", end = " ")
+    for j in range(i):
+        print("*", end = " ")
+    for j in range(i+1):
+        print("*", end = " ")
+    
+    print()
