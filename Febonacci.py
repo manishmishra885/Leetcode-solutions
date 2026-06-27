36bc0d1de07efd6847d47e0ebd5cc519c1386792
Manish Mishra <mmanishmishra885@gmail.com>
2026-06-27 14:40:06 +0530

    Create Febonacci.py

---
+++ Febonacci.py
@@ -0,0 +1,19 @@
+n = 10
+a=0 
+b=1
+
+for i in range (n):
+    print(a, end = " ")
+    c = a+b 
+    a = b 
+    b = c 
+
+
+# RECURSIVE 
+def febo (n):
+    if n <= 1 :
+        return n 
+    
+    return febo(n-1) + febo (n-2)
+for i in range (7):
+    # print(febo(i), end = " ")
